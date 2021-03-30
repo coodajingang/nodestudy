c21dc01f781a2806be516ad8a501a6182571bd7a
dutengxiao <dutengxiao.zb@ccbft.com>
2021-03-30 18:28:37 +0800

    xx

---
+++ calendar/tscalendar/src/jd.ts
@@ -0,0 +1,11 @@
+export interface JdDate {
+    year?: number;
+    month?: number;
+    day?: number;
+    hour?: number;
+    minute?: number;
+    second?: number;
+    jd?: number;
+}
+
+export class Jd
